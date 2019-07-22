462f5f5cf6e7fd3f68e5c654b67a9b2ab0843255
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-22 04:02:12 +0100

    creating & running both migration & seeder files

--- ExpensesAPI/ExpensesAPI/Models/Entry.cs
+++ ExpensesAPI/ExpensesAPI/Models/Entry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace ExpensesAPI.Models
 {
     public class Entry
     {
+        [Key]
+        public int id { get; set; }
         public string Description { get; set; }
         public bool IsExpense { get; set; }
 
