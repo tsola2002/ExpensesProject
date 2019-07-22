ca56bd6f41a1eeff8edd10bdfeaa9346b6c872f9
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-22 07:16:24 +0100

    added controller file for entries

---
+++ ExpensesAPI/ExpensesAPI/Controllers/EntriesController.cs
@@ -0,0 +1,30 @@
+﻿using ExpensesAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpensesAPI.Controllers
+{
+    public class EntriesController : ApiController
+    {
+        public IHttpActionResult GetEntries()
+        {
+            try
+            {
+                using (var context = new AppDbContext())
+                {
+                    var entries = context.Entries.ToList();
+                    return Ok(entries);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
