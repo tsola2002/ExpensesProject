3235ab6d8b7e194d1b3a7e02449ac25fe21aa4d2
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-22 02:08:00 +0100

    added entry context file

--- ExpensesAPI/ExpensesAPI/Models/Entry.cs
+++ ExpensesAPI/ExpensesAPI/Models/Entry.cs
@@ -7,5 +7,9 @@ namespace ExpensesAPI.Models
 {
     public class Entry
     {
+        public string description { get; set; }
+        public bool IsExpense { get; set; }
+
+        public double price { get; set; }
     }
 }
