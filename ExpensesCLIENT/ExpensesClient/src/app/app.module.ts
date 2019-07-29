f1822c388e7fec0fb0d29c17678afa74b0398345
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-29 06:00:20 +0100

    added component files to the project

--- ExpensesCLIENT/ExpensesClient/src/app/app.module.ts
+++ ExpensesCLIENT/ExpensesClient/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
