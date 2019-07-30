16ad2aa8428597e08bed5fee637370d40f5fdbdc
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-30 19:48:06 +0100

    display data to the users

--- ExpensesCLIENT/ExpensesClient/src/app/entries/entries.component.ts
+++ ExpensesCLIENT/ExpensesClient/src/app/entries/entries.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EntryService } from '../entry.service';
+import { MatTableDataSource } from '@angular/material';
+import { EntryElement } from '../interfaces/EntryElement';
 
 
 @Component({
@@ -9,11 +11,21 @@ import { EntryService } from '../entry.service';
 })
 export class EntriesComponent implements OnInit {
 
+   //we need to inject our entry.service.ts into the constructor
   constructor(private service:EntryService) { }
 
+  displayedColumns: string[] = ['Description', 'IsExpense', 'Value'];
+  dataSource;
+
   ngOnInit() {
+     //we need to trigger the service method(getAll) from with this component
+    //the getAll method returns an observable, so we need to use the subscribe method to get the response object
+    //then we log the response
     this.service.getAll().subscribe((data) => {
-      console.log('Results -', data);    
+      console.log('Results -', data);
+       //we need to fill the data from the subscribe method
+      //we need to define we receive an ElementEntry Datatype & the data will be an array of entry elements
+      this.dataSource = new MatTableDataSource<EntryElement>(data as EntryElement[]);    
     })
   }
 
---
+++ ExpensesCLIENT/ExpensesClient/src/app/interfaces/EntryElement.ts
@@ -0,0 +1,7 @@
+//this interface will have all the properties of our data
+//it will be made accessible through a class export
+export interface EntryElement{
+    Description:string;
+    IsExpense:boolean;
+    Value:number;
+}
