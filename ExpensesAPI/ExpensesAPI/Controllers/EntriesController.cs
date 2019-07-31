1a684be17e5af7d6f36879921112dd3a594cc098
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-07-31 05:09:13 +0100

    create web API HttpPost Endpoint

--- ExpensesAPI/ExpensesAPI/Controllers/EntriesController.cs
+++ ExpensesAPI/ExpensesAPI/Controllers/EntriesController.cs
@@ -1,4 +1,5 @@
 ﻿using ExpensesAPI.Data;
+using ExpensesAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,34 @@ namespace ExpensesAPI.Controllers
                 return BadRequest(ex.Message);
             }
 
+        }
+
+        //send entry data as part of body request
+        //datatype is Entry with defined name as entry
+        [HttpPost]
+        public IHttpActionResult PostEntry([FromBody]Entry entry)
+        {
+            //return badrequest if model is not valid
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            //enclose statement in try/catch incase something goes wrong
+            try
+            {
+                //create reference to out appDbcontext
+                using (var context = new AppDbContext())
+                {
+                    //add the entry using the reference
+                    context.Entries.Add(entry);
+                    context.SaveChanges();
+
+                    return Ok("Entry was created!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //return the message of the exception
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
